093338c32a477d3b69fe3fd56513d3faf4c3a169
Dominic Nancekievill <dominic.n@gmail.com>
2021-04-20 13:58:07 +0100

    Reduced logging to what makes sense.

--- index.js
+++ index.js
@@ -9,24 +9,26 @@ const fs = require('fs').promises;
 
         const owner = github.context.repo.owner;
         const repo = github.context.repo.repo;
-        console.log(`Owner = ${owner}`);
-        console.log(`Repo = ${repo}`);
-
         const path = core.getInput('path');
-        console.log(`Path = ${path}`);
 
-        console.log(`Event name = ${github.context.eventName}`);
+        const eventName = github.context.eventName;
+        if (eventName !== 'release') {
+            core.setFailed(`Should be run by a release event but event name = ${eventName}`);
+        }
         const releaseId = github.context.payload.release.id;
-        console.log(`Release id = ${releaseId}`);
+
+        const pathParts = path.split('/');
+        const filename = pathParts[pathParts.length - 1];
 
         const fileContent = await fs.readFile(path);
         console.log(`Read ${fileContent.length} bytes from ${path}`);
 
+        console.log(`Uploading as ${filename}`);
         await octokit.rest.repos.uploadReleaseAsset({
             owner: owner,
             repo: repo,
             release_id: releaseId,
-            name: "aname",
+            name: filename,
             data: fileContent
         });
 
