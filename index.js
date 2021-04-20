f5603bf6d32a0245192dff6135ffda1f629eade2
Dominic Nancekievill <dominic.n@gmail.com>
2021-04-20 10:07:48 +0100

    Missed the async wrapper.

--- index.js
+++ index.js
@@ -2,31 +2,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs').promises;
 
-try {
-    const token = core.getInput('token');
-    const octokit = github.getOctokit(token);
+(async () => {
+    try {
+        const token = core.getInput('token');
+        const octokit = github.getOctokit(token);
 
-    const owner = github.context.repo.owner;
-    const repo = github.context.repo.repo;
-    console.log(`Owner = ${owner}`);
-    console.log(`Repo = ${repo}`);
+        const owner = github.context.repo.owner;
+        const repo = github.context.repo.repo;
+        console.log(`Owner = ${owner}`);
+        console.log(`Repo = ${repo}`);
 
-    const path = core.getInput('path');
-    console.log(`Path = ${path}`);
+        const path = core.getInput('path');
+        console.log(`Path = ${path}`);
 
-    console.log(`Event name = ${github.context.eventName}`);
-    const releaseId = github.context.payload.release.id;
-    console.log(`Release id = ${releaseId}`);
+        console.log(`Event name = ${github.context.eventName}`);
+        const releaseId = github.context.payload.release.id;
+        console.log(`Release id = ${releaseId}`);
 
-    const fileContent = await fs.readFile(path);
+        const fileContent = await fs.readFile(path);
 
-    await octokit.rest.repos.uploadReleaseAsset({
-        owner: owner,
-        repo: repo,
-        release_id: releaseId,
-        data: fileContent
-    });
+        await octokit.rest.repos.uploadReleaseAsset({
+            owner: owner,
+            repo: repo,
+            release_id: releaseId,
+            data: fileContent
+        });
 
-} catch (error) {
-    core.setFailed(error.message);
-}
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
