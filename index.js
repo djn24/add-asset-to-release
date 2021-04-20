cafd3c411475d4ca3db5be8f2429aa61e7cc0080
Dominic Nancekievill <dominic.n@gmail.com>
2021-04-20 10:04:29 +0100

    Trying the upload. File handling is a bit of a guess.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs').promises;
 
 try {
     const token = core.getInput('token');
     const octokit = github.getOctokit(token);
+
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+    console.log(`Owner = ${owner}`);
+    console.log(`Repo = ${repo}`);
 
     const path = core.getInput('path');
     console.log(`Path = ${path}`);
@@ -11,6 +17,15 @@ try {
     console.log(`Event name = ${github.context.eventName}`);
     const releaseId = github.context.payload.release.id;
     console.log(`Release id = ${releaseId}`);
+
+    const fileContent = await fs.readFile(path);
+
+    await octokit.rest.repos.uploadReleaseAsset({
+        owner: owner,
+        repo: repo,
+        release_id: releaseId,
+        data: fileContent
+    });
 
 } catch (error) {
     core.setFailed(error.message);
