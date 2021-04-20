51bd22f5632ed58be89fa5bbf97582ac04fb9e2b
Dominic Nancekievill <dominic.n@gmail.com>
2021-04-20 08:48:19 +0100

    Creating octokit, getting release id.

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+    const sourceToken = core.getInput('source-token');
+    const octokit = github.getOctokit(sourceToken);
+
     const path = core.getInput('path');
     console.log(`Path = ${path}`);
+
+    console.log(`Event name = ${github.context.eventName}`);
+    const releaseId = github.context.payload.release.id;
+    console.log(`Release id = ${releaseId}`);
+
 } catch (error) {
     core.setFailed(error.message);
 }
